8a722bec898408a8468f395085d53acb51f1b078
Chandu <chandu@Chandus-MacBook-Pro.local>
2020-12-13 22:45:32 -0500

    2

--- solution.py
+++ solution.py
@@ -14,13 +14,13 @@ def welcome_assignment_answers(question):
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = "b21bed4510d4c731cf6a8afe2c2bc071"
+        answer = "42b76fe51778764973077a5a94056724"
     elif question ==  "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "Yes"
     elif question ==  "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = "4"
+        answer = "5"
     elif question ==  "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = "3"
+        answer = "4"
     return(answer)
 # Complete all the questions.
 
