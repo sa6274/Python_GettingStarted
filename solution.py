5a87a90dd6852fa2144e3d1344d1ac2009195c77
Chandu <chandu@Chandus-MacBook-Pro.local>
2020-12-13 22:51:17 -0500

    5

--- solution.py
+++ solution.py
@@ -6,17 +6,17 @@ def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No1"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No2"
+        answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "No3"
+        answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No4"
+        answer = "No"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
     elif question ==  "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question ==  "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = int("5")
     elif question ==  "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
